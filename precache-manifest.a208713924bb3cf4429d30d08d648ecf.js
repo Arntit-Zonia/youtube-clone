b869df6a8d68cdcc0e4c3a7f2c450fb949cf8b8d
Arntit-Zonia <arditzonia@gmail.com>
2019-09-23 11:24:09 +0100

    Updates

---
+++ precache-manifest.a208713924bb3cf4429d30d08d648ecf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c663001fce670e8ed987f6a27f5b92",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "14fb60770b9afdd11e77",
+    "url": "/youtube-clone/static/css/main.6c4d9a3d.chunk.css"
+  },
+  {
+    "revision": "77fdd57f01b0e457e4ca",
+    "url": "/youtube-clone/static/js/2.a295959a.chunk.js"
+  },
+  {
+    "revision": "14fb60770b9afdd11e77",
+    "url": "/youtube-clone/static/js/main.c9cc2961.chunk.js"
+  },
+  {
+    "revision": "5b6d52ae34e3e3e4dd92",
+    "url": "/youtube-clone/static/js/runtime~main.8754cbdc.js"
+  }
+]);
